2af437f99d2cdadfdc97eae6eff245aaf4f27ba3
shanesalphie <94833880+shanesalphie@users.noreply.github.com>
2022-07-20 13:01:04 +0530

    Add files via upload

---
+++ queuelinkedlist.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node 
+{
+	int data ;
+	struct node *next ;
+};
+struct node *head=NULL,*pos=NULL,*temp=NULL,*del=NULL,*new;
+
+
+void enqueue (int x)
+{	
+		if (head==NULL)
+		{
+			head=(struct node *)malloc(sizeof(struct node));
+			head -> data =x;
+		}
+		else
+		{
+			new=(struct node *)malloc(sizeof(struct node));
+			new-> data=x;
+			new->next=head;
+			head=new;
+		}
+}
+void dequeue()
+{
+	if (head==NULL)
+	{
+		printf("No elements !");
+		printf("\n");
+	}
+	else
+	{
+		del=head;
+		head=head->next;
+		free(del);
+		pos=head;
+		if(head==NULL)
+		{
+			printf("No elements to print...");
+			printf("\n");
+		}
+		else
+		{
+			printf("The elements after deletion are:\n");
+			while(pos !=NULL)
+			{
+				printf("%d\n",pos->data);
+				pos=pos->next;
+			}
+			printf("\n");
+		}
+	}
+	
+}
+
+void display()
+{
+	if(head==NULL)
+	{
+		printf("No elements present..");
+		printf("\n");
+	}
+	else
+	{
+
+		pos=head;
+		printf("\nElements are :\n");
+		while (pos !=NULL)
+		{
+			printf("%d\n",pos -> data);
+			pos =pos  -> next;
+		}
+		printf("\n");
+	}
+}
+
+void main()
+{
+	int ch,val;
+	printf("MENU");
+	printf("\n1.ENQUEUE \n2.DEQUEUE \n3.DISPLAY \n4.EXIT\n");
+	while(1)
+	{
+		printf("Enter your choice:");
+		scanf("%d",&ch);
+		switch (ch)
+		{
+			case 1:
+				{
+					printf("Enter the value to be pushed:");
+				      	scanf("%d",&val);
+				      	enqueue(val);
+				} 
+				break;  			
+			case 2: 
+				{
+					dequeue();
+				}
+				break;
+			case 3:
+				{
+					display();
+				}
+				break;
+			case 4:
+				{
+					printf("Thankyou !!\n");
+					exit(0);
+				}
+			default:
+				printf("Option not found !");
+		}
+	}
+}
